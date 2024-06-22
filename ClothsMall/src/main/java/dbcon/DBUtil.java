ff1ae5a9a1a6cbd0e0e1e59bd6f2b2722223821d
Siu Ahn <jumia011@gmail.com>
2024-06-22 15:09:35 +0900

    [create] create index.jsp [update] update DBUtil.java

--- ClothsMall/src/main/java/dbcon/DBUtil.java
+++ ClothsMall/src/main/java/dbcon/DBUtil.java
@@ -1,6 +1,9 @@
 package dbcon;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -18,5 +21,67 @@ public class DBUtil {
 		}
 		System.out.println("Connection 생성성공");
 		return conn;
+	}
+
+	public static void dbReleaseClose(ResultSet rs, Statement stmt, Connection conn) {
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			rs = null;
+		}
+
+		try {
+			if (stmt != null) {
+				stmt.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			stmt = null;
+		}
+		try {
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			conn = null;
+		}
+	}
+
+	public static void dbReleaseClose(Statement stmt, Connection conn) {
+		try {
+			if (stmt != null) {
+				stmt.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		try {
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			conn = null;
+		}
+	}
+
+	public static void dbReleaseClose(Connection conn) {
+		try {
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			conn = null;
+		}
 	}
 }
